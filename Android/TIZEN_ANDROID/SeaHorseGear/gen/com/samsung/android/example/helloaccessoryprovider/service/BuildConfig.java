e8c6be752879766b4be7a217e6d113e8becfcff2
k2s <tt1661@naver.com>
2014-06-01 18:40:58 +0900

    GearApp porject name modification. 프로젝트 이름 변경.

--- Android/TIZEN_ANDROID/SeaHorseGear/gen/com/samsung/android/example/helloaccessoryprovider/service/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.samsung.android.example.helloaccessoryprovider.service;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
